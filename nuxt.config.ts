52810d598606f0ad0af039b002354000ce3308d1
turret-detected <andrew.mohnkern@gmail.com>
2025-08-10 10:26:46 -0400

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  ssr: true,
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  ssr: false,
   nitro: {
     static: true
   }
